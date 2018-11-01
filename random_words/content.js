cee98218a808bfa8a6cae9a2a5f134985ad0ad19
Max <max.spring@ya.ru>
2018-11-01 03:17:43 +0300

    new hardly working version

--- random_words/content.js
+++ random_words/content.js
@@ -1,26 +1,83 @@
-// не оч умная версия, которая находит только теги из фиксированного списка,
-// и тупо хайлйатит их, возможно чтото ломая
+//  еще одна не оч умная версия, которая находит текстовые ноды
+//  и хайлйатит их, чтото ломая почти всегда
 
 var color = "#ffff00";
 var prob = 0.1;
-var cnt = 0;
-var tags = ["h1", "h2", "h3", "h4", "p", "a"];
+//var tags = ["h1", "h2", "h3", "h4", "p", "a"];
+
+
+var text_nodes = [];
+function dfs(node) {
+    for (var child = node.firstChild; child; child=child.nextSibling) {
+        if (child.nodeType == 3) {
+            text_nodes.push(child);
+        }
+        dfs(child);
+    }
+}
+
+dfs(document);
+
+//console.log(text_nodes);
+
+for (node of text_nodes) {
+    parent = node.parentNode;
+    parent_tag = parent.nodeName;
+    if (parent_tag == "SCRIPT") {
+        continue;
+    }
+    var text = node.innerText || node.textContent;
+    if (!text) {
+        continue;
+    }
+    words = text.split(' ');
+    var changed = false;
+    for (var i = 0, length = words.length; i < length; i += 1) {
+        if (Math.random() >= prob) {
+            continue;
+        }
+        changed = true;
+        words[i] = "<span style='background-color: " + color + ";'>" + words[i] + "</span>";
+    }
+    
+    if (changed) {
+        new_node = document.createElement(parent_tag);
+        new_node.innerHTML = words.join(delimiter=' ');
+        parent.replaceChild(new_node, node);
+    }
+}
+
+
+
+
+
+
+
+/*
 for (tag of tags) {
     for (node of document.getElementsByTagName(tag)) {
         
         var text = node.innerText || node.textContent;
-        if (!text) { // || node.hasChildNodes(
+        if (!text) {
             cnt++;
             continue;
         }
         words = text.split(' ');
+        var changed = false
         for (var i = 0, length = words.length; i < length; i += 1) {
             if (Math.random() >= prob) {
                 continue;
             }
+            changed = true;
             words[i] = "<span style='background-color: " + color + ";'>" + words[i] + "</span>";
-            node.innerHTML = words.join(delimiter = ' ');
         }
+        
+        if (changed) {
+        }
+        new_node = document.createElement(tag)
+        new_node.innerHTML = words.join(delimiter = ' ');
+        parent = node.parentNode;
+        parent.replaceChild(new_node, node);
     }
 }
-console.log('skipped ', cnt, 'nodes');
+console.log('skipped ', cnt, 'nodes');*/
