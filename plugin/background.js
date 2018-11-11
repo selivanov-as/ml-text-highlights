8f3fe04dbb1ebc04b2ddc51d9dd9f3c9bfe1ded8
kirill <kbulgakov@yahoo.com>
2018-11-11 21:30:53 +0300

    Generate idf object and highlight all words with tfidf >= 0.3

--- plugin/background.js
+++ plugin/background.js
@@ -1,6 +1,8 @@
 const highlight = `
     const random = () => Math.floor(Math.random()*2);
-    const regex = /[^а-яa-z0-9]/gi;
+    const regex = /[^а-я0-9]/gi;
+    const tf = {};
+    let wordsAmount = 0;
     
     const walkDOM = (node, func) => {
         func(node);
@@ -21,11 +23,31 @@ const highlight = `
 
         if (!text.length) return; 
         
-        const instance = new Mark(node);
-        random() % 2 === 0 ? instance.mark(text, node) : null;   
+        text.forEach(word => {
+            if (!tf[word.toLowerCase()]) {
+                tf[word.toLowerCase()] = [1, [node]];
+                wordsAmount++;
+            } else {
+                tf[word.toLowerCase()][0]++;
+                tf[word.toLowerCase()][1].push(node)
+            }
+        });
+        // random() % 2 === 0 ? instance.mark(text, node) : null;   
     }
     
     walkDOM(document.body, highlightFunc);
+    const multiplier = 1000;
+    const instance = new Mark(document);
+    Object.keys(tf).forEach(term => {
+        const term_idf = idf[term] ? idf[term] : 0;
+        const tf_idf = tf[term][0] / wordsAmount * term_idf * multiplier;
+        console.log(term, tf_idf);
+        if (tf_idf >= 0.3) {
+            tf[term][1].forEach(context => instance.mark(term, context))
+            // instance.mark(term, document);
+        }
+    });
+    
 `;
 
 chrome.browserAction.onClicked.addListener(function (tab) {
