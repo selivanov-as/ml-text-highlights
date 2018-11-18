ea52d600acc1c588259fd49881392c4776b05cb0
Kirill <bulgakovk@users.noreply.github.com>
2018-11-18 17:56:49 +0300

    Fix highlighting
    Prevent sub-words highlighting and fix regexp to correct token splitting

--- plugin/background.js
+++ plugin/background.js
@@ -1,6 +1,6 @@
 const highlight = `
     const random = () => Math.floor(Math.random()*2);
-    const regex = /[^а-я0-9]/gi;
+    const regex = /[^аАбБвВгГдДеЕёЁжЖзЗиИйЙкКлЛмМнНоОпПрРсСтТуУфФхХцЦчЧшШщЩъЪыЫьЬэЭюЮяЯ0-9]/gi;
     const tf = {};
     let wordsAmount = 0;
     
@@ -32,7 +32,6 @@ const highlight = `
                 tf[word.toLowerCase()][1].push(node)
             }
         });
-        // random() % 2 === 0 ? instance.mark(text, node) : null;   
     }
     
     walkDOM(document.body, highlightFunc);
@@ -41,10 +40,8 @@ const highlight = `
     Object.keys(tf).forEach(term => {
         const term_idf = idf[term] ? idf[term] : 0;
         const tf_idf = tf[term][0] / wordsAmount * term_idf * multiplier;
-        console.log(term, tf_idf);
         if (tf_idf >= 0.3) {
-            tf[term][1].forEach(context => instance.mark(term, context))
-            // instance.mark(term, document);
+            tf[term][1].forEach(context => instance.mark(term, {accuracy: "exactly"}))
         }
     });
     
