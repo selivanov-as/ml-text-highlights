95794add54868678a8ca3c4dde3b40ebf7062bea
kirill <kbulgakov@yahoo.com>
2019-02-13 10:41:02 +0300

    Refactor README: How to count AC/Prec/Recall/F1 metrics

--- server/script.py
+++ server/script.py
@@ -1,5 +1,7 @@
-from server.tf_idf_normalized import tf_idf_normalized
-from server.samples_raw import samples_raw
+from tf_idf_normalized import tf_idf_normalized
+from samples_raw import samples_raw
+
+METHOD = tf_idf_normalized
 
 
 def clean_words(words):
@@ -14,7 +16,7 @@ def clean_words(words):
 
 
 def count_acc(words, results):
-    wrong, right = 0, 0
+    right = 0
 
     if len(words) != len(results):
         raise Exception("Result length isn't correct")
@@ -25,10 +27,44 @@ def count_acc(words, results):
 
         if highlightActual == highlightPredicted:
             right += 1
-        else:
-            wrong += 1
 
     return right / len(words)
+
+
+def count_precision(words, results):
+    tp, tn, fp, fn = 0, 0, 0, 0
+
+    if len(words) != len(results):
+        raise Exception("Result length isn't correct")
+
+    for i, result in enumerate(results):
+        highlightActual = words[i][0] == "@"
+        highlightPredicted = result["highlight"]
+
+        if highlightActual == highlightPredicted:
+            tp += 1
+        if highlightActual == False and highlightPredicted == True:
+            fp += 1
+
+    return tp / (tp + fp)
+
+
+def count_recall(words, results):
+    tp, tn, fn = 0, 0, 0
+
+    if len(words) != len(results):
+        raise Exception("Result length isn't correct")
+
+    for i, result in enumerate(results):
+        highlightActual = words[i][0] == "@"
+        highlightPredicted = result["highlight"]
+
+        if highlightActual == highlightPredicted:
+            tp += 1
+        if highlightActual == True and highlightPredicted == False:
+            fn += 1
+
+    return tp / (tp + fn)
 
 
 print("Results for version with POS tagging:")
@@ -36,17 +72,27 @@ for sample in samples_raw:
     words = list(filter(lambda x: len(x) > 0, sample["words"]))
     words_cleaned = clean_words(words)
 
-    results = tf_idf_normalized(words_cleaned)
+    results = METHOD(words_cleaned)
     highlighted = list(filter(lambda x: x["highlight"], results))
-    print("Highlight predictions:", list(map(lambda x: x["word"], highlighted)))
-    print("Accuracy =", count_acc(words, results))
+    print(f"Highlight predictions: {list(map(lambda x: x['word'], highlighted))}")
+    print(f"Accuracy = {count_acc(words, results)}")
+    precision = count_precision(words, results)
+    recall = count_recall(words, results)
+    print(f"Precision = {precision}")
+    print(f"Recall = {recall}")
+    print(f"F-score = {2 * precision * recall / (precision + recall)}")
 
-print("\nResults for version without POS tagging")
-for sample in samples_raw:
-    words = list(filter(lambda x: len(x) > 0, sample["words"]))
-    words_cleaned = clean_words(words)
-
-    results = tf_idf_normalized(words_cleaned, use_pos_tagging=False)
-    highlighted = list(filter(lambda x: x["highlight"], results))
-    print("Highlight predictions:", list(map(lambda x: x["word"], highlighted)))
-    print("Accuracy =", count_acc(words, results))
+    # print("\nResults for version without POS tagging")
+    # for sample in samples_raw:
+    #     words = list(filter(lambda x: len(x) > 0, sample["words"]))
+    #     words_cleaned = clean_words(words)
+    #
+    #     results = METHOD(words_cleaned, use_pos_tagging=False)
+    #     highlighted = list(filter(lambda x: x["highlight"], results))
+    #     print(f"Highlight predictions: {list(map(lambda x: x['word'], highlighted))}")
+    #     print(f"Accuracy = {count_acc(words, results)}")
+    #     precision = count_precision(words, results)
+    #     recall = count_recall(words, results)
+    #     print(f"Precision = {precision}")
+    #     print(f"Recall = {recall}")
+    #     print(f"F-score = {2 * precision * recall / (precision + recall)}")
