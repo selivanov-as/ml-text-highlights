e3089a7dabb1cfbdf10f55ed3711d36a0c1ed260
Max <max.spring@ya.ru>
2019-03-18 13:24:35 +0300

    edit lambda src for w2v

--- lambda_src_embeddings/main.py
+++ lambda_src_embeddings/main.py
@@ -2,12 +2,10 @@ import string
 import pymorphy2
 import json
 import itertools
-import requests
 
 morph = pymorphy2.MorphAnalyzer()
-# from rus_preprocessing_mystem import (tag_mystem, mystem2upos)
 
-NORMS_FILE, LEMMR = ('norms_ruscorpora_upos_cbow_300_20_2019.json', 'mystem')
+NORMS_FILE, LEMMR = ('./norms_sg_full.json', 'pymorphy')
 
 PUNCTUATION = string.punctuation + ''.join([
     '–', '—', '‒', '−', '«', '»', '\xa0', '°', '′', '\u2009', '\u200e',
@@ -16,6 +14,7 @@ PUNCTUATION = string.punctuation + ''.join([
 
 with open(NORMS_FILE) as f:
     vector_norms = json.load(f)
+
 
 def tokenize_lemmatize_input(inp, lem='pymorphy'):
     """
@@ -38,36 +37,40 @@ def tokenize_lemmatize_input(inp, lem='pymorphy'):
                 for word in cur_tokens]
             tokens.append(cur_tokens)
             normalized_tokens.append(cur_normalized_tokens)
-
-    # elif lem == 'mystem':
-    #     for text in texts:
-    #         cur_tokens, cur_normalized_tokens = tag_mystem(
-    #             text, mapping=mystem2upos
-    #         )
-    #         tokens.append(cur_tokens)
-    #         normalized_tokens.append(cur_normalized_tokens)
     else:
-        assert False, 'wrong lemmatiser, expected on of ["mystem", "pymorphy"]'
+        assert False, 'wrong lemmatiser, expected on of ["pymorphy"]'
     return tokens, normalized_tokens
 
 
 def important_words_to_spans(important_words, input,
-                             grouped_words, spaces_skipped=True):
+                             grouped_words, spaces_skipped=True,
+                             use_normalised=False, grouped_norm_words=None,
+                             important_norm_words=None):
     """
     :param important_words: flat set of important (not normalized) words
     :param input: server input in unified format
     :param grouped_words: document tokens grouped by node (list of lists)
     :param spaces_skipped: if spaces skipped in tokenization
+    :param use_normalised: use set of normalised important words to determine
+    spans (helps preventing mistakes due to unstable normalization)
+    :param grouped_norm_words: normalized tokens grouped by node (must be
+    provided if use_normalized == True)
+    :param important_norm_words: set of important normalized words (must be
+    provided if use_normalized == True)
     :return: list of lists_of_spans_in_each_node;
     span is a tuple (begin_ind, end_ind) -- halfopen interval of a highlight
     """
     grouped_spans = []
-    for node, words in zip(input, grouped_words):
+    for i, (node, words) in enumerate(zip(input, grouped_words)):
         cur_pos = 0
         text = node['text']
         cur_spans = []
-        for word in words:
-            if word in important_words:
+        for j, word in enumerate(words):
+            if (
+                    (use_normalised
+                     and grouped_norm_words[i][j] in important_norm_words)
+                    or (not use_normalised and word in important_words)
+            ):
                 beg = text.find(word, cur_pos)
                 cur_pos = end = beg + len(word)
                 cur_spans.append((beg, end))
@@ -77,10 +80,11 @@ def important_words_to_spans(important_words, input,
         grouped_spans.append(cur_spans)
     return grouped_spans
 
+
 def choose_n_important(sorted_pairs, min_share=0.05, max_share=0.4):
     if not sorted_pairs:
         return 0
-    declines = [(prev[1] / cur[1], i)
+    declines = [(prev[1][0] / cur[1][0], i)
                 for i, (prev, cur)
                 in enumerate(
                     zip(sorted_pairs, sorted_pairs[1:]),
@@ -91,12 +95,14 @@ def choose_n_important(sorted_pairs, min_share=0.05, max_share=0.4):
     _, n_important = max(declines[min_ind : max_ind + 1])
     return n_important
 
+
 def handler(event, context):
-    inp = json.loads(event["body"])
+    inp = json.loads(event["body"])['texts']
 
     tokens, normalized_tokens = tokenize_lemmatize_input(inp["texts"])
 
-    norms = {normalized_token: vector_norms.get(normalized_token, float('-inf'))
+    norms = {normalized_token: (vector_norms.get(normalized_token,
+                                                 float('-inf')),)
              for normalized_token in itertools.chain(*normalized_tokens)}
 
     vector_norms_sorted = sorted(norms.items(),
@@ -116,9 +122,14 @@ def handler(event, context):
                         in zip(cur_tokens, cur_normalized_tokens)
                         if normalized_token in important_normalized_tokens}
 
+    spans = important_words_to_spans(
+            important_tokens, inp, tokens,
+            spaces_skipped=(LEMMR == 'pymorphy'),
+            use_normalised=True, grouped_norm_words=normalized_tokens,
+            important_norm_words=important_normalized_tokens)
+
     return {
         'statusCode': 200,
         'headers': {'Content-Type': 'application/json'},
-        'body': json.dumps(important_words_to_spans(important_tokens, inp["texts"], tokens))
-        # 'body': json.dumps(inp)
+        'body': json.dumps(spans)
     }
