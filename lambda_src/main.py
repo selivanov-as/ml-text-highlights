f3a6e07e9422cea1ccaea574e540cd2c637f9aa8
Max <max.spring@ya.ru>
2019-03-18 15:42:18 +0300

    create venv for lambda

---
+++ lambda_src/main.py
@@ -0,0 +1,63 @@
+import json
+
+from gensim.summarization import summarize
+
+
+def joined_spans_to_grouped_spans(spans, texts, dlm, joined):
+    borders = []
+    end = -len(dlm)
+    for text in texts:
+        beg = end + len(dlm)
+        end = beg + len(text)
+        borders.append((beg, end))
+
+    assert len(borders) == len(texts), (len(borders), len(texts))
+    assert borders[-1][1] == len(joined), (borders[-1][1], len(joined))
+
+    cur_ind = 0
+    grouped_spans = []
+
+    for (beg, end) in borders:
+        start_ind = cur_ind
+        while cur_ind < len(spans) and spans[cur_ind][1] < end:
+            cur_ind += 1
+        cur_spans = spans[start_ind : cur_ind]
+        if cur_ind < len(spans) and spans[cur_ind][0] < end:
+            # span is divided between text nodes
+            cur_spans.append((spans[cur_ind][0], end))
+        cur_spans = [tuple(max(0, x - beg) for x in span)
+                            for span in cur_spans]
+        grouped_spans.append(cur_spans)
+
+    assert len(texts) == len(grouped_spans), (len(texts), len(grouped_spans))
+    return grouped_spans
+
+
+def handler(event, context):
+    inp = json.loads(event["body"])['texts']
+
+    texts = [x['text'] for x in inp]
+    dlm = ''
+    joined = dlm.join(texts)
+
+    sentences = summarize(joined, ratio=0.2, split=True)
+
+    cur_start = 0
+    spans = []
+    for sent in sentences:
+        beg = joined.find(sent, cur_start)
+        if beg == -1:
+            # print('====Not Found!!!====')
+            # print(repr(sent))
+            # print(repr(joined[cur_start : cur_start + 100]))
+            continue
+        cur_start = end = beg + len(sent)
+        spans.append((beg, end))
+
+    grouped_spans = joined_spans_to_grouped_spans(spans, texts, dlm, joined)
+
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'application/json'},
+        'body': json.dumps(grouped_spans)
+    }
