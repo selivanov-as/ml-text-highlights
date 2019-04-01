d3ea003e37746722a4b8bdcaff10e2c9557b5103
kirill <kbulgakov@yahoo.com>
2019-04-01 11:34:18 +0300

    Fix CORS in gensim_sentences model

--- lambda_src/main.py
+++ lambda_src/main.py
@@ -81,6 +81,11 @@ def handler(event, context):
     )
     return {
         'statusCode': 200,
-        'headers': {'Content-Type': 'application/json'},
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': True,
+            'Access-Control-Allow-Headers': 'Content-Type'
+        },
         'body': json.dumps(grouped_spans)
     }
