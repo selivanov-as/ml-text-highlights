0779e760e3642cdadc75c31f441396d07706f58c
Max <max.spring@ya.ru>
2019-05-13 02:59:16 +0300

    change share

--- lambda_src/tf_idf_normalized.py
+++ lambda_src/tf_idf_normalized.py
@@ -14,7 +14,7 @@ with open('./normalized_idf.json') as f:
 with open("./stopwords.txt") as f:
     stop_words_list = f.readlines()
 
-SHARE = 0.2
+SHARE = 0.4
 
 stop_words = {}
 for x in stop_words_list:
