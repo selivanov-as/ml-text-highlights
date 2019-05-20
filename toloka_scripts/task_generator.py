c1f47f60a8c873303bc3e0c9aa7624061dc4ef15
Max <max.spring@ya.ru>
2019-05-20 16:48:22 +0300

    add 0.3-0.7 share check comparison

--- toloka_scripts/task_generator.py
+++ toloka_scripts/task_generator.py
@@ -4,11 +4,13 @@ import os
 screenshots_folder = '../screenshot_emails/'
 prefix = 'highlightdisk/screenshot_emails/'  # proxy and folder in yadisk
 files = os.listdir(screenshots_folder + 'random')
-exclude_files = {'letter1.png', 'letter7.png',
+exclude_files = {}
+'''{'letter1.png', 'letter7.png',
                  'letter10.png','letter14.png',
                  'letter17.png', 'letter18.png'}  # уберем те которые одинаковые для алгоритмов
+'''
 files = [file for file in files if file not in exclude_files]
-pairs = [('tf_idf_pos_share_06', 'tf_idf_pos_share_07')]  #, ('tf_idf_embeddings', 'gensim_sentences'), 'gensim_keywords', 'embeddings']
+pairs = [('tf_idf_pos', 'tf_idf_pos_share_07')]  #, ('tf_idf_embeddings', 'gensim_sentences'), 'gensim_keywords', 'embeddings']
 
 honeypot_algs = ['tf_idf_embeddings',
                  'tf_idf_custom', 'tf_idf_pos']
@@ -19,7 +21,7 @@ honeypot_files = random.choices(files, k=round(
 ))
 
 
-with open('tf_idf_pos_share_06_vs_tf_idf_pos_share_07.tsv', 'w') as f:
+with open('tf_idf_pos_vs_tf_idf_pos_share_07.tsv', 'w') as f:
     f.write('INPUT:image_left	INPUT:image_right	GOLDEN:result	HINT:text\n')
     for pair in pairs:
         filelists = [os.listdir(screenshots_folder + alg) for alg in pair]
